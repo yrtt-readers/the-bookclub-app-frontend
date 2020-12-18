f0043dcf85fece8b5c4200ed299e00b0899ca24c
notwaving <samanthaburke.uk@gmail.com>
2020-12-18 16:39:28 +0000

    adds Header component

--- src/App.js
+++ src/App.js
@@ -3,76 +3,12 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
-      <header className='header'>
-        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-          <div className='container-fluid'>
-            <a className='navbar-brand' href='#'>
-              The BookClub
-            </a>
-            <button
-              className='navbar-toggler'
-              type='button'
-              data-bs-toggle='collapse'
-              data-bs-target='#navbarSupportedContent'
-              aria-controls='navbarSupportedContent'
-              aria-expanded='false'
-              aria-label='Toggle navigation'
-            >
-              <span className='navbar-toggler-icon'></span>
-            </button>
-            <div
-              className='collapse navbar-collapse'
-              id='navbarSupportedContent'
-            >
-              <ul className='navbar-nav me-auto mb-2 mb-lg-0 d-flex'>
-                <li className='nav-item'>
-                  <a className='nav-link' aria-current='page' href='#'>
-                    Home
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link' aria-current='page' href='#'>
-                    About us
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a
-                    className='nav-link'
-                    aria-current='page'
-                    href='/index.html'
-                  >
-                    Donate Books
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link activate' aria-current='page' href='#'>
-                    Request Book
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link' aria-current='page' href='#'>
-                    Find Event
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link' aria-current='page' href='#'>
-                    Contact us
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link' aria-current='page' href='#'>
-                    Log in / Sign up
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
+      <Header />
       <section className='container container-margin'>
         <div className='row g-3'>
           <h1 className='text-center'>Books available</h1>
@@ -106,7 +42,7 @@ function App() {
           </div>
 
           <div className='col-auto'>
-            <Button variant="primary">Search</Button>
+            <Button variant='primary'>Search</Button>
           </div>
         </div>
         <div className='row booklist'>
