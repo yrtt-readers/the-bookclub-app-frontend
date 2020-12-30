279279b4088709605f5286bee2d32f75d1622360
sciamanna <rebeca.sciamanna@me.com>
2020-12-30 15:39:19 +0000

    updated test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+ render(<BrowserRouter> <App /> </BrowserRouter>);
+  //render(<App />);
+  //const brandElement = screen.getByText(/The BookClub/i);
+  const brandElement = screen.getAllByText(/The BookClub/i);
+  console.log(brandElement);
+  //expect(brandElement).toBeInTheDocument();
+  //expect(brandElement).length.toBe(1)
+
 });
