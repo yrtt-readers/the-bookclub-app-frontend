603da0f93dec0f04db401e94eb172da82f2f2ab7
Winndie Fan <winndie@gmail.com>
2021-01-02 22:51:11 +0000

    added test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { screen, render, cleanup } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+  render(<App />, container);
+});
+
+afterEach(() => { cleanup });
+
+describe('App', () => {
+
+  test('renders heading component', () => {
+    expect(screen.getByRole('heading').textContent).toBe('Books available')
+  });
+
+  test('renders footer component', () => {
+    expect(screen.getByRole('contentinfo').textContent).toBe('The Book Club!')
+  });
+
 });
