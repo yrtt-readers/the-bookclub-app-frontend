808e4e3a8d0adb81c8027f5e0460a0161378517d
Winndie Fan <winndie@gmail.com>
2021-01-02 22:20:45 +0000

    implemented state, props, ajax and event handling

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,48 +1,59 @@
 // /* eslint-disable */
 import PropTypes from 'prop-types';
 import React from 'react';
+import $ from 'jquery';
 import './Book.css';
-import { booksData } from './BooksData.js';
+// import { booksData } from './BooksData.js';
 
 function Book({ isbn, stocks, setStocks }) {
 
-  const bookData = booksData.filter(book => book.isbn === isbn);
-  console.log(bookData)
+  // const bookData = booksData.filter(book => book.isbn === isbn);
+  //  console.log(bookData)
 
-  function setBookToStocks(e) {
-    if (e.target.className === 'btn btn-primary request')
-      setStocks(stocks => [...stocks, { isbn: isbn, qty: -1 }])
-    else if (e.target.className === 'btn btn-primary donate')
-      setStocks(stocks => [...stocks, { isbn: isbn, qty: +1 }])
+$.ajax({
+  url: 'https://yrtt-readers.github.io/the-bookclub/assets/data/books.json',
+  async: false,
+  success: data => {
+    try {
+      $.each(data.filter(book => book.isbn === isbn)[0],
+        (key, val) => { sessionStorage.setItem(isbn + '-' + key, val) })
+    } catch (e) { console.log(e) }
   }
+})
 
-  return (
-    <div className='col-lg-4 col-sm-6 book'>
-      <img
-        className='img-thumbnail'
-        src='https://covers.openlibrary.org/b/id/9943475-L.jpg'
-        alt='book-image-not-found'
-      />
-      <p className='book-description'>
-        <strong>Wider Than the Sky</strong>
-      </p>
-      <p className='book-description'>
-        <strong>Author names</strong>{' '}
-      </p>
-      <p className='book-description'>
-        A diverse anthology of poems by William Shakespeare...
-  </p>
-      <p className='book-description'>Book Quantity :
-      {stocks.reduce((sum, stock) => sum + stock.qty, 0)}
-      </p>
-      <p className='book-description'>Post Code</p>
-      <p className='book-description'>
-        <a href='#'>More info</a>
-      </p>
-      <button onClick={setBookToStocks} className='btn btn-primary request'>Request</button>
-      <button onClick={setBookToStocks} className='btn btn-primary donate'>Donate</button>
-    </div>
-  );
+function setBookToStocks(e) {
+  if (e.target.className === 'btn btn-primary request')
+    setStocks(stocks => [...stocks, { isbn: isbn, qty: -1 }])
+  else if (e.target.className === 'btn btn-primary donate')
+    setStocks(stocks => [...stocks, { isbn: isbn, qty: +1 }])
+}
+
+
+return (
+  <div className='col-lg-4 col-sm-6 book'>
+    <img
+      className='img-thumbnail'
+      src={sessionStorage.getItem(isbn + '-' + 'thumbnail')}
+      alt='book-image-not-found'
+    />
+    <p className='book-description'>
+      <strong>{sessionStorage.getItem(isbn + '-' + 'book_name')}</strong>
+    </p>
+    <p className='book-description'>
+      <strong>Author names</strong>{' '}
+    </p>
+    <p className='book-description'>{sessionStorage.getItem(isbn + '-' + 'description')}</p>
+    <p className='book-description'>Book Quantity : 
+    {stocks.reduce((sum, stock) => sum + stock.qty, 0)}
+    </p>
+    <p className='book-description'>Post Code</p>
+    <p className='book-description'>
+      <a href='#'>More info</a>
+    </p>
+    <button onClick={setBookToStocks} className='btn btn-primary request'>Request</button>
+    <button onClick={setBookToStocks} className='btn btn-primary donate'>Donate</button>
+  </div>
+);
 }
 
 Book.propTypes = {
--- src/components/Book/BooksData.js
+++ src/components/Book/BooksData.js
@@ -1,54 +1,54 @@
 export const booksData = [
     {
-        isbn: '9780789411464',
+        isbn: 9780789411464,
         book_name: 'Mad Jack',
         description: 'N/A',
         thumbnail: 'https://covers.openlibrary.org/b/id/551629-L.jpg'
     },
     {
-        isbn: '9780060217860',
+        isbn: 9780060217860,
         book_name: 'Wider Than the Sky',
         description: 'A diverse anthology of poems by William Shakespeare, Dr. Seuss, Emily Dickinson, and many others.',
         thumbnail: 'https://covers.openlibrary.org/b/id/9943475-L.jpg'
     },
     {
-        isbn: '9780806919317',
+        isbn: 9780806919317,
         book_name: 'Balloon',
         description: "Look out the window--everything''s white with snow! What better day for Benjamin Bear to go out and play? Join him and his dad in this winter wonderland as they make a snowman, go skiing and sledding, and have a great time...in spite of a few little slips and slides and falls!",
         thumbnail: 'https://covers.openlibrary.org/b/id/2643729-L.jpg'
     },
     {
-        isbn: '9780875349343',
+        isbn: 9780875349343,
         book_name: 'Mathmania',
         description: "It's math with a Puzzlemania twist! Puzzle masters and fanatics will love these challenging books, all based on a mathematical theme. Each book includes brainteasers, secret codes, visual puzzles, logic puzzles, number searches, and more. These books equal a load of thought-provoking fun. An answer key is provided in the back of each book.",
         thumbnail: 'https://covers.openlibrary.org/b/id/667024-L.jpg'
     },
     {
-        isbn: '9780893751159',
+        isbn: 9780893751159,
         book_name: 'Brave Little Tailor',
         description: "A tailor who kills seven flies with one blow outwits the king and earns half his kingdom and his daughter's hand in marriage.",
         thumbnail: 'https://covers.openlibrary.org/b/id/9600218-L.jpg'
     },
     {
-        isbn: '9780689853944',
+        isbn: 9780689853944,
         book_name: 'Red Dog',
         description: "In the rugged Wyoming territory, the red pup is Adam's best friend.\nAdam and his family live in a lonely cabin in the mountains, facing the dangers of the wilderness alone. One day, Adam¹s stepfather announces that he must leave for a weeklong trip to Cheyenne. Adam is put in charge of the family. Everything goes smoothly until three cutthroat gold prospectors come crashing into the cabin and hold the family at gunpoint.\nLate that night, Adam manages to escape. Unfortunately, the men let the red pup loose, and the one thing that dog does best is track Adam...Will the pup lead the men to Adam? And if he does, can Adam still save his family?",
         thumbnail: 'https://covers.openlibrary.org/b/id/437328-L.jpg'
     },
     {
-        isbn: '9780866228312',
+        isbn: 9780866228312,
         book_name: 'Hamsters',
         description: 'N/A',
         thumbnail: 'https://covers.openlibrary.org/b/id/657610-L.jpg'
     },
     {
-        isbn: '9780911981568',
+        isbn: 9780911981568,
         book_name: 'Arizona',
         description: 'N/A',
         thumbnail: 'https://covers.openlibrary.org/b/id/9670138-L.jpg'
     },
     {
-        isbn: '9780816741342',
+        isbn: 9780816741342,
         book_name: 'Where Is Carmen Sandiego?',
         description: "A skilled thief on a mysterious mission, Carmen Sandiego is endlessly pursued by ACME and Interpol. But the woman in the red fedora is always one step ahead! In this novelization, based on the Netflix animated series, Carmen shares her own backstory for the first time ever. Now, it's time to find out.... Who in the world is Carmen Sandiego. ",
         thumbnail: 'https://covers.openlibrary.org/b/id/1551657-L.jpg'
