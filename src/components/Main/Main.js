701d21028660e6a8ffad2d4f600a5a7071e6b1f4
notwaving <samanthaburke.uk@gmail.com>
2021-01-04 21:41:32 +0000

    adds withRouter to export

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import Callback from './Callback/Callback';
 
 import Home from '../../pages/Home/Home';
 import Donate from '../../pages/Donate/Donate';
 import Signup from '../../pages/Signup/Signup';
-import Login from '../../pages/Login/Login'
+import Login from '../../pages/Login/Login';
 
 function Main() {
-    return (
-        <Switch>
-            <Route exact path='/' component={Home}></Route>
-            <Route exact path='/signup' component={Signup}></Route>
-            <Route exact path='/login' component={Login}></Route>
-            <Route exact path='/donate' component={Donate}></Route>
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route exact path='/' component={Home}></Route>
+      <Route exact path='/signup' component={Signup}></Route>
+      <Route exact path='/login' component={Login}></Route>
+      <Route exact path='/donate' component={Donate}></Route>
+    </Switch>
+  );
 }
 
-export default Main;
+export default withRouter(Main);
