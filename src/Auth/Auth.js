c4eb8663882c77866c3a443c8558203a90f40dbb
notwaving <samanthaburke.uk@gmail.com>
2021-01-04 20:20:33 +0000

    adds .env.local variables to Auth.js

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -4,9 +4,9 @@ export default class Auth {
   constructor() {
     this.auth0 = new auth0.WebAuth({
       // the following three lines MUST be updated
-      domain: '<AUTH0_DOMAIN>',
+      domain: 'process.env.REACT_APP_AUTH0_DOMAIN',
       audience: 'https://<AUTH0_DOMAIN>/userinfo',
-      clientID: '<AUTH0_CLIENT_ID>',
+      clientID: 'process.env.REACT_APP_AUTH0_CLIENT_ID',
       redirectUri: 'http://localhost:3000/callback',
       responseType: 'token id_token',
       scope: 'openid profile',
