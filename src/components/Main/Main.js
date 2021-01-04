fe9bb08a9fe86e4b8b43030680e155759f6a954d
notwaving <samanthaburke.uk@gmail.com>
2021-01-04 21:53:43 +0000

    completes Main.js auth0 update for now

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,55 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { withRouter } from 'react-router';
-import Callback from './Callback/Callback';
+import Callback from '../Callback/Callback.js';
 
 import Home from '../../pages/Home/Home';
 import Donate from '../../pages/Donate/Donate';
 import Signup from '../../pages/Signup/Signup';
 import Login from '../../pages/Login/Login';
 
-function Main() {
+function LoginPage(props) {
+  const { authenticated } = props;
+
+  const logout = () => {
+    props.auth.logout();
+    props.history.push('/login');
+  };
+
+  if (authenticated) {
+    const { name } = props.auth.getProfile();
+    return (
+      <div>
+        <h1>Hello! Glad to see you back, {name}.</h1>
+        <button onClick={logout}>Log out</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Please log in.</h1>
+      <button onClick={props.auth.login}>Log in</button>
+    </div>
+  );
+}
+
+function Main(props) {
+  const authenticated = props.auth.isAuthenticated();
+
   return (
     <Switch>
-      <Route exact path='/' component={Home}></Route>
+      <Route exact path='/' component={Home}>
+        <Callback auth={props.auth} />
+      </Route>
       <Route exact path='/signup' component={Signup}></Route>
-      <Route exact path='/login' component={Login}></Route>
+      <Route exact path='/login' component={Login}>
+        <LoginPage
+          authenticated={authenticated}
+          auth={props.auth}
+          history={props.history}
+        />
+      </Route>
       <Route exact path='/donate' component={Donate}></Route>
     </Switch>
   );
