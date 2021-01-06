8baa0757651f41b673a97394197fb5f03f5bd408
Winndie Fan <winndie@gmail.com>
2021-01-06 15:05:28 +0000

    modiflied sessionStorage format bookData example key=book.9780060217860 value={"isbn":9780060217860,"book_name":"Wider Than the Sky","description":"A diverse anthology of poems by William Shakespeare, Dr. Seuss, Emily Dickinson, and many others.","thumbnail":"https://covers.openlibrary.org/b/id/9943475-L.jpg"}

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -4,26 +4,28 @@ import React from 'react';
 import $ from 'jquery';
 import './Book.css';
 
-const bookComponentList = [
-  { mode: -1, label: null, className: 'none' },
-  { mode: 0, label: 'Request', className: 'btn btn-primary request' },
-  { mode: 1, label: 'Donate', className: 'btn btn-primary donate' },
-]
+const bookComponent = new Map()
+bookComponent.set(-1, { label: null, className: 'none' })
+bookComponent.set(0, { label: 'Request', className: 'btn btn-primary request' })
+bookComponent.set(1, { label: 'Donate', className: 'btn btn-primary donate' })
+
 
 function Book({ mode, isbn, stocks, setStocks }) {
 
-  const bookComponent = bookComponentList.find(v => v.mode == mode)
+  (sessionStorage.getItem('book.' + isbn) === null) ?
+    $.ajax({
+      url: 'https://yrtt-readers.github.io/the-bookclub/assets/data/books.json',
+      async: false,
+      success: data => {
+        try {
+          sessionStorage
+            .setItem('book.' + isbn,
+              JSON.stringify(data.filter(book => book.isbn === isbn)[0]))
+        } catch (e) { console.log(e) }
+      }
+    }) : null
 
-  $.ajax({
-    url: 'https://yrtt-readers.github.io/the-bookclub/assets/data/books.json',
-    async: false,
-    success: data => {
-      try {
-        $.each(data.filter(book => book.isbn === isbn)[0],
-          (key, val) => { sessionStorage.setItem(isbn + '-' + key, val) })
-      } catch (e) { console.log(e) }
-    }
-  })
+  const bookData = JSON.parse(sessionStorage.getItem('book.' + isbn))
 
   function setBookToStocks(e) {
     if (e.target.className === 'btn btn-primary request')
@@ -36,16 +38,16 @@ function Book({ mode, isbn, stocks, setStocks }) {
     <div className='col-lg-4 col-sm-6 book'>
       <img
         className='img-thumbnail'
-        src={sessionStorage.getItem(isbn + '-' + 'thumbnail')}
+        src={bookData.thumbnail}
         alt='book-image-not-found'
       />
       <p className='book-description'>
-        <strong>{sessionStorage.getItem(isbn + '-' + 'book_name')}</strong>
+        <strong>{bookData.book_name}</strong>
       </p>
       <p className='book-description'>
         <strong>Author names</strong>{' '}
       </p>
-      <p className='book-description'>{sessionStorage.getItem(isbn + '-' + 'description')}</p>
+      <p className='book-description'>{bookData.description}</p>
       <p className='book-description'>Book Quantity: {stocks.reduce((sum, stock) => sum + stock.qty, 0)}
       </p>
       <p className='book-description'>Post Code</p>
@@ -53,8 +55,8 @@ function Book({ mode, isbn, stocks, setStocks }) {
         <a href='#'>More info</a>
       </p>
       <button onClick={setBookToStocks}
-        className={bookComponent.className}>
-        {bookComponent.label}
+        className={bookComponent.get(mode).className}>
+        {bookComponent.get(mode).label}
       </button>
     </div>
   )
