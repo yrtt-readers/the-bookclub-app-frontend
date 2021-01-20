80282d9f73f6ad6851989e02b9ebd1d4e9015d84
Winndie Fan <winndie@gmail.com>
2021-01-20 20:18:31 +0000

    Update BookList.test.js

--- src/components/BookList/BookList.test.js
+++ src/components/BookList/BookList.test.js
@@ -1,30 +1,60 @@
+/* eslint-disable */
 import BookList from './BookList';
-import { screen, render, cleanup, fireEvent } from '@testing-library/react';
-import { createMemoryHistory } from 'history'
-import { Router } from 'react-router-dom'
+import { render, cleanup, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
-let container = null;
-let history = createMemoryHistory()
-let mode=0
+const bookNames = [
+  "Wider Than the Sky",
+  "Mad Jack",
+  "Benjamin in the snow",
+  "Mathmania",
+  "Brave Little Tailor",
+  "Red Dog",
+  "Hamsters",
+  "Arizona",
+  "Where Is Carmen Sandiego?"
+]
+
+const bookAuthors = [
+  "Scott Elledge",
+  "Susan Mayes",
+  "Anne Leblanc",
+  "Highlights",
+  "Andrej Dugin (Author), Olga Dugina",
+  "Bill Wallace",
+  "Percy Parslow",
+  "USA",
+  "James Buckley Jr. and Michael S. Teitelbaum"
+]
+
+let mode=0, container, divBookName, divAuthor
 
 beforeEach(() => {
   container = document.createElement("div")
-  document.body.appendChild(container);
+  document.body.appendChild(container)
   render(
-    <Router history={history}>
-      <BookList mode={mode}/>
-    </Router>, container
+    <BrowserRouter >
+      <BookList mode={mode} />
+    </BrowserRouter>, container
   )
+  divBookName = screen.queryAllByTestId('book_name')
+  divAuthor = screen.queryAllByTestId('book_author')
 })
 
 afterEach(() => { cleanup });
 
 describe('BookList', () => {
 
-  test('renders sorting dropdown', () => {
-    expect(screen.getByTestId('sort').textContent).toBe('Sort by')
-    // expect(screen.queryByTestId('book_name').textContent).toBe('Mad Jack')
+  test('unsorted book names', () => {
+    expect(JSON.stringify(bookNames) === 
+           JSON.stringify(divBookName.map(v => v.textContent))
+          ).toBeTruthy()
+  })
 
+  test('unsorted book authors', () => {
+    expect(JSON.stringify(bookAuthors) === 
+           JSON.stringify(divAuthor.map(v=>v.textContent))
+          ).toBeTruthy()
   })
 
 })
