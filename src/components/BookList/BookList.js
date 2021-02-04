bcb45116d90aa7b2ba2fbb304e5a791d6c13c18b
Winndie Fan <winndie@gmail.com>
2021-02-04 21:41:38 +0000

    test git action

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -72,7 +72,7 @@ function BookList({ mode }) {
     else {
       if (mode < 2) {
         $.ajax({
-          url: 'https://yrtt-readers.github.io/the-bookclub/assets/data/stocks_new.json',
+          url: 'https://yrtt-readers.github.io/the-bookclub/assets/data/stocks.json',
           async: false,
           dataType: 'json',
           success: data => {
